77ca633c61e1eacc09a6d5eed44ebf7b39762b32
Marcelo Morais <m2marcelo@gmail.com>
2020-07-01 00:28:58 +0200

    Frontend: src/components adding create category component
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

---
+++ frontend/src/components/CreateCategory.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react'
+import { Form, Button } from 'semantic-ui-react'
+import { createBlogCategory } from '../api/blog-categories-api'
+import Auth from '../auth/Auth'
+
+interface CreateCategoryProps {
+  auth: Auth
+}
+
+interface CreateCategoryState {
+  title: string
+  content: string
+  uploadingCategory: boolean
+}
+
+export class CreateCategory extends React.PureComponent<
+  CreateCategoryProps,
+  CreateCategoryState
+> {
+  state: CreateCategoryState = {
+    title: '',
+    content: '',
+    uploadingCategory: false
+  }
+
+  handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ title: event.target.value })
+  }
+
+  handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ content: event.target.value })
+  }
+
+  handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+
+    try {
+      if (!this.state.title || !this.state.content) {
+        alert('Title and content are mandatory')
+        return
+      }
+
+      this.setUploadState(true)
+      const category = await createBlogCategory(this.props.auth.getIdToken(), {
+        title: this.state.title,
+        content: this.state.content
+      })
+
+      console.log('Created content', category)
+
+      alert('Category was created!')
+    } catch (e) {
+      alert('Could not upload a category: ' + e.message)
+    } finally {
+      this.setUploadState(false)
+    }
+  }
+
+  setUploadState(uploadingGroup: boolean) {
+    this.setState({
+      uploadingCategory: true
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Create and store new category</h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>Title</label>
+            <input
+              placeholder="Title for the new category"
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Description</label>
+            <input
+              placeholder="Content for this category"
+              value={this.state.content}
+              onChange={this.handleContentChange}
+            />
+          </Form.Field>
+          {this.renderButton()}
+        </Form>
+      </div>
+    )
+  }
+
+  renderButton() {
+    return (
+      <Button loading={this.state.uploadingCategory} type="submit">
+        Create
+      </Button>
+    )
+  }
+}
