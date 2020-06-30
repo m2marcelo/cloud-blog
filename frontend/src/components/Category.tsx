43f7d3f15e97cdd393d978daca7b318d884eba93
Marcelo Morais <m2marcelo@gmail.com>
2020-07-01 00:40:34 +0200

    Frontend: src/components adding category component
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

---
+++ frontend/src/components/Category.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { Card } from 'semantic-ui-react'
+import { BlogCategoriesModel } from '../types/BlogCategoriesModel'
+import { Link } from 'react-router-dom'
+
+interface CategoryCardProps {
+  category: BlogCategoriesModel
+}
+
+interface CategoryCardState {
+}
+
+export class Group extends React.PureComponent<CategoryCardProps, CategoryCardState> {
+
+  render() {
+    return (
+      <Card>
+        <Card.Content>
+          <Card.Header>
+            <Link to={`/images/${this.props.category.id}`}>{this.props.category.title}</Link>
+          </Card.Header>
+          <Card.Description>{this.props.category.content}</Card.Description>
+        </Card.Content>
+      </Card>
+    )
+  }
+}
