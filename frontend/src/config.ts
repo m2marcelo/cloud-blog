9fc3a34365ba32ad8434400fad429899e5fb9cd8
Marcelo Morais <m2marcelo@gmail.com>
2020-06-30 22:01:39 +0200

    Frontend: updating config.ts with AWS and Auth0 data
    Signed-off-by: Marcelo Morais <m2marcelo@gmail.com>

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,12 @@
-export const apiEndpoint = '...'
+const apiId = '61413a3u8k'
+const regionId = 'us-west-2'
+
+export const apiEndpoint = `https://${apiId}.execute-api.${regionId}.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain: 'dev-m2marcelo.eu.auth0.com',            // Auth0 domain
+  clientId: 'SoDtAwrIjY9VGA6cghPtJzi7kvQKqiO7',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
+
